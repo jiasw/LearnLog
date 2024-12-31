3c5ffc1d411a1b0c457ece29b5f72d705f87c8a7
jiasw <jswjia@163.com>
2024-12-31 17:10:54 +0800

    init

--- WPFControl/WPF.UI/MainWindow.xaml.cs
+++ WPFControl/WPF.UI/MainWindow.xaml.cs
@@ -24,64 +24,10 @@ namespace WPF.UI
         }
 
        
-
-        /// <summary>
-        /// UI变化
-        /// </summary>
-        /// <param name="bState"></param>
-        private void StartChange(bool bState)
-        {
-            if (bState)
-                btnStart.Content = "停止";
-            else
-                btnStart.Content = "开始";
-        }
-
-        private void StartAnimation()
-        {
-            DoubleAnimation animation = new DoubleAnimation();
-            animation.From = 0;
-            animation.To = 1;
-            animation.Duration = new Duration(new System.TimeSpan(0, 0, 8));
-            //circleProgressBar.BeginAnimation(CircularProgressBar.CurrentValueProperty, animation);
-        }
-
-        /// <summary>
-        /// 缓动动画
-        /// </summary>
-        private void StartEaseAnimation()
-        {
-            DoubleAnimation animation = new DoubleAnimation();
-            animation.From = 0;
-            animation.To = 1;
-            animation.Duration = new Duration(new System.TimeSpan(0, 0, 8));
-            var ease = new CubicEase();
-            ease.EasingMode = EasingMode.EaseOut;
-            animation.EasingFunction =ease;
-            
-            //circleProgressBar.BeginAnimation(CircularProgressBar.CurrentValueProperty, animation);
-        }
-
-        private void keyFrameAnimation_Completed()
-        {
-            DoubleAnimationUsingKeyFrames animation = new DoubleAnimationUsingKeyFrames();
-            animation.Duration = new Duration(new System.TimeSpan(0, 0, 8));
-            animation.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromTimeSpan(new System.TimeSpan(0, 0, 0))));
-            animation.KeyFrames.Add(new LinearDoubleKeyFrame(0.25, KeyTime.FromTimeSpan(new System.TimeSpan(0, 0, 2))));
-            animation.KeyFrames.Add(new LinearDoubleKeyFrame(0.5, KeyTime.FromTimeSpan(new System.TimeSpan(0, 0, 4))));
-            animation.KeyFrames.Add(new LinearDoubleKeyFrame(0.75, KeyTime.FromTimeSpan(new System.TimeSpan(0, 0, 6))));
-            animation.KeyFrames.Add(new LinearDoubleKeyFrame(1, KeyTime.FromTimeSpan(new System.TimeSpan(0, 0, 8))));
-            //circleProgressBar.BeginAnimation(CircularProgressBar.CurrentValueProperty, animation);
-        }
-
-
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-           Task.Factory.StartNew(() => ShowDashBoard());
-
-           keyFrameAnimation_Completed();
+           
             //Task.Factory.StartNew(() => StartCircleAnimation());
             //PathControl pathControl = new PathControl();
             //pathControl.ShowDialog();
@@ -121,30 +67,9 @@ namespace WPF.UI
 
         }
 
-
-        private void StartCircleAnimation()
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            int i = 1;
-            while (true)
-            {
-                
-                
-                CircularProcess.Dispatcher.Invoke(() =>
-                {
-                    CircularProcess.CurrentValue = i / 100;
-                });
-                i++;
-                if (i >= 100)
-                {
-                    i = 1;
-                }
-                Thread.Sleep(1000);
-            }
-            
+            Task.Factory.StartNew(() => ShowDashBoard());
         }
-
-
-
-
     }
 }
