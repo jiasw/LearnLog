1a0c98fcdef6890ee39f3c79a29e79956d9a5e8b
jiasw <jswjia@163.com>
2025-01-11 17:43:28 +0800

    添加了学习算法的代码记录

---
+++ algo/HelloAlgo/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HelloAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        static List<string[]> Alllist(List<string> ints)
+        {
+           List<string[]> ints1 = new List<string[]>();
+
+
+            bool[] arrselected = new bool[ints.Count];
+
+
+
+
+
+
+
+            return ints1;
+        }
+
+
+        
+        static List<string[]> Backtracking(string[] arrselect, string[] result,int index, List<string[]>)
+        {
+            if (arrselect.Length==1)
+            {
+                result[index] = arrselect[0];
+                return result;
+            }
+            for (int i = 0; i < arrselect.Length; i++)
+            {
+                string curr = arrselect[i];
+                result
+
+            }
+            
+
+        }
+
+
+    }
+}
